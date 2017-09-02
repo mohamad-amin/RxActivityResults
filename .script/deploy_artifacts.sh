24627111213298c4d4ad1753482fb3039bbe0628
MohamadAmin <mohamadamin@cafebazaar.ir>
2017-09-02 20:36:57 +0430

    updated deploy_artifacts.sh

--- .script/deploy_artifacts.sh
+++ .script/deploy_artifacts.sh
@@ -5,7 +5,7 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="mohamad-amin/RxActivityResults"
+SLUG="mohamad-amin/rxactivityresults"
 BRANCH="master"
 JDK="oraclejdk8"
 
