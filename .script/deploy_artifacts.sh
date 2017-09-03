0ecf988fb57eedc6368e50b242244c45a9ce81bc
MohamadAmin <mohamadamin@cafebazaar.ir>
2017-09-03 13:46:18 +0430

    Fixed jfrog uploader

--- .script/deploy_artifacts.sh
+++ .script/deploy_artifacts.sh
@@ -21,6 +21,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying artifacts..."
-  ./gradlew publishFromCI --info
+  ./gradlew bintrayUpload --info
   echo "Artifacts deployed!"
 fi
